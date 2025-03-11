a88b30f332294af7e0658d1e7ab2ba360a764f89
Matheesha Wickramaarachchi <ashaninduwara2018@gmail.com>
2025-03-11 15:26:32 +0530

    Front end form data extraction Done

---
+++ assignment-metana/src/app/services/extractService.ts
@@ -0,0 +1,30 @@
+// services/uploadService.ts
+
+export const extractCvData = async (file: File) => {
+    try {
+      // Check if the file is a PDF before sending it as application/pdf
+      if (file.type !== 'application/pdf') {
+        throw new Error('Only PDF files are allowed.')
+      }
+  
+      const formData = new FormData()
+      formData.append('file', file)
+  
+      const response = await fetch('http://127.0.0.1:3000/pdf-to-text', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/pdf', // Explicitly set the Content-Type to application/pdf
+        },
+        body: formData,
+      })
+  
+      if (!response.ok) {
+        throw new Error('Failed to extract CV data.')
+      }
+  
+      return await response.json()
+    } catch (error) {
+      throw new Error(error.message || 'An error occurred during the extraction process.')
+    }
+  }
+  
